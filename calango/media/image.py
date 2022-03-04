aa4fa94c3235a1111d1c28cc9f4b7edb531d76ab
jlsneto <leitejoab@gmail.com>
2022-03-04 09:42:39 -0300

    improve: Improve Image

--- calango/media/image.py
+++ calango/media/image.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
 
 import math
+import time
 from abc import ABC, abstractmethod
-from typing import Union, Tuple
+from typing import Union, Tuple, Sequence
 
 import cv2
 import cereja as cj
 from matplotlib import pyplot as plt
 import numpy as np
 
-__all__ = ['Image']
+__all__ = ['Image', 'Video']
 
 try:
     # noinspection PyUnresolvedReferences
@@ -39,6 +40,8 @@ class _InterfaceImage(ABC):
 
 
 class Image(_InterfaceImage):
+    _GRAY_SCALE = 'GRAY_SCALE'
+    IMAGE_FORMATS = {'.jpg', '.jpeg', '.png'}
 
     def __init__(self, image_or_path: Union[str, np.ndarray], channels='BGR'):
         self._path = None
@@ -50,35 +53,40 @@ class Image(_InterfaceImage):
             self._channels = 'BGR'
         else:
             assert isinstance(image_or_path, np.ndarray) and len(image_or_path.shape) == 3, 'Image format is invalid'
-            self._data = image_or_path
-            self._channels = channels.upper()
+            self._data = image_or_path.copy()
+            if self.shape[-1] == 1:
+                self._channels = self._GRAY_SCALE
+            else:
+                self._channels = channels.upper()
 
     def _get_channel_data(self, c):
+        if self._channels == self._GRAY_SCALE:
+            raise ValueError('The image is Gray Scale')
         assert isinstance(c, str), 'send str R, G or B for channel.'
         return self.data[:, :, self._channels.index(c.upper())]
 
     @property
-    def data(self):
+    def data(self) -> np.ndarray:
         return self._data
 
-    def flip(self):
+    def flip(self) -> Image:
         self._data = cv2.flip(self._data, 1)
         return self
 
     @property
-    def r(self):
+    def r(self) -> np.ndarray:
         return self._get_channel_data('R')
 
     @property
-    def g(self):
+    def g(self) -> np.ndarray:
         return self._get_channel_data('G')
 
     @property
-    def b(self):
+    def b(self) -> np.ndarray:
         return self._get_channel_data('B')
 
     @property
-    def shape(self):
+    def shape(self) -> Tuple:
         return self.data.shape
 
     @property
@@ -91,10 +99,10 @@ class Image(_InterfaceImage):
 
     @property
     def center(self) -> Tuple[int, int]:
-        return self.width // 2, self.height // 2
+        return int(self.width // 2), int(self.height // 2)
 
     @property
-    def min_len(self):
+    def min_len(self) -> int:
         return min(self._get_h_w(self.shape))
 
     @classmethod
@@ -110,16 +118,30 @@ class Image(_InterfaceImage):
             return n_w, math.floor(o_h / (o_w / n_w))
         return math.floor(o_w / (o_h / n_h)), n_h
 
-    def bgr_to_rgb(self):
+    def bgr_to_rgb(self) -> Image:
         if self._channels == 'BGR':
             self._data = cv2.cvtColor(self._data, cv2.COLOR_BGR2RGB)
             self._channels = 'RGB'
+        else:
+            raise Exception("Image isn't BGR")
         return self
 
-    def rgb_to_bgr(self):
+    def to_gray_scale(self) -> Image:
+        if self._channels == 'BGR':
+            self._data = cv2.cvtColor(self._data, cv2.COLOR_BGR2GRAY)
+        elif self._channels == 'RGB':
+            self._data = cv2.cvtColor(self._data, cv2.COLOR_RGB2GRAY)
+        else:
+            raise Exception("Image isn't RGB or BGR")
+        self._channels = self._GRAY_SCALE
+        return self
+
+    def rgb_to_bgr(self) -> Image:
         if self._channels == 'RGB':
             self._data = cv2.cvtColor(self._data, cv2.COLOR_RGB2BGR)
             self._channels = 'BGR'
+        else:
+            raise Exception("Image isn't RGB")
         return self
 
     def resize(self, shape: Union[tuple, list], keep_scale: bool = False) -> Image:
@@ -160,17 +182,17 @@ class Image(_InterfaceImage):
         self._data = im_crop[start[1]:end[1], start[0]:end[0]]
         return self
 
-    def prune(self):
+    def prune(self) -> Image:
         min_len = self.min_len
         return self.crop_by_center((min_len, min_len))
 
-    def show(self):
+    def plot(self):
         if ON_COLAB_JUPYTER:
             from google.colab.patches import cv2_imshow
             cv2_imshow(self.data)
         else:
             if self._channels == 'BGR':
-                plt.imshow(cv2.cvtColor(self._data, cv2.COLOR_BGR2RGB))
+                plt.imshow(cv2.cvtColor(self.data, cv2.COLOR_BGR2RGB))
             else:
                 plt.imshow(self.data)
             plt.show()
@@ -201,48 +223,108 @@ class Image(_InterfaceImage):
         plt.show()
 
     def draw_text(self, text,
-                  font=cv2.FONT_HERSHEY_PLAIN,
                   pos='left_bottom',
+                  font=cv2.FONT_HERSHEY_PLAIN,
                   font_scale=1,
                   font_thickness=1,
                   text_color=(0, 255, 0),
                   text_color_bg=(0, 0, 0)
-                  ):
+                  ) -> Image:
+        """
+        Write text on this Image and returns self.
+
+        :param text: a string
+        :param pos: region on write text in the image
+        :param font: cv2 font index. cv2.FONT_HERSHEY_PLAIN is default.
+        :param font_scale: size of font
+        :param font_thickness: thickness of font
+        :param text_color: BGR color
+        :param text_color_bg: BGR color
+        :return: Image
+        """
 
         text = str(text)
         text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
-        k = int(min((self.width, self.height)) * 0.05)
+        k = int(self.min_len * 0.03)
+
+        w_top_r_limit = (self.width - text_size[0]) - k
+        w_center_limit = int((self.width - text_size[0]) // 2) + k
+        h_bottom_limit = (self.height - text_size[1]) - k
+
         pos = {'left_top':      (k, k),
-               'left_bottom':   (k, self.height - k*2),
-               'right_top':     ((self.width - text_size[0]) - k, k),
-               'right_bottom':  ((self.width - text_size[0]) - k, self.height - k*2),
-               'center_top':    (((self.width - text_size[0]//2) // 2 - k), k),
-               'center_bottom': (((self.width - text_size[0]//2) // 2 - k), self.height - k*2)
+               'left_bottom':   (k, h_bottom_limit),
+               'right_top':     (w_top_r_limit, k),
+               'right_bottom':  (w_top_r_limit, h_bottom_limit),
+               'center_top':    (w_center_limit, k),
+               'center_bottom': (w_center_limit, h_bottom_limit)
                }.get(pos, (k, k))
 
         x, y = pos
 
         text_w, text_h = text_size
         cv2.rectangle(self.data, pos, (x + text_w, y + text_h), text_color_bg, -1)
-        cv2.putText(self.data, text, (x, y + text_h + font_scale - 1), font, font_scale, text_color, font_thickness)
+        cv2.putText(self.data, text, (x, y + text_h), font, font_scale, text_color, font_thickness)
 
         return self
 
     def gaussian_pyramid(self, levels=3, *args, **kwargs):
 
         img = self.data.copy()
-        pyramid = [img]
+        pyramid = [Image(img)]
         while levels >= 1:
             img = cv2.pyrDown(img, *args, **kwargs)
-            pyramid.append(img)
+            pyramid.append(Image(img))
             levels -= 1
         return pyramid
 
-    def laplacian_pyramid(self, levels=3):
+    def laplacian_pyramid(self, levels=3) -> Sequence[Image]:
         gaussian_pyramid = self.gaussian_pyramid(levels)
         pyramid = []
         for i in range(levels, 0, -1):
-            GE = cv2.pyrUp(gaussian_pyramid[i])
-            L = cv2.subtract(gaussian_pyramid[i - 1], GE)
+            GE = cv2.pyrUp(gaussian_pyramid[i].data)
+            GE = cv2.resize(GE, gaussian_pyramid[i - 1].data.shape[::-1][1:])
+            L = Image(cv2.subtract(gaussian_pyramid[i - 1].data, GE))
             pyramid.append(L)
         return pyramid
+
+    @classmethod
+    def images_from_dir(cls, dir_path, ext='.jpg'):
+        dir_path = cj.Path(dir_path)
+        return [cls(im_p.path) for im_p in dir_path.list_files(ext=ext)]
+
+
+class Video:
+    def __init__(self, data):
+        try:
+            self._n_frames = len(data)
+        except TypeError:
+            self._n_frames = None
+
+        self._data = data
+
+    def show(self, fps=30, window_name='Video', write_info=False):
+        start_time = time.time()
+        for current_frame, image in enumerate(self._data, start=1):
+            start = time.time()
+
+            if not isinstance(image, Image):
+                image = Image(image)
+            if write_info:
+                current_time = time.time() - start_time
+                image.draw_text(
+                    f'Frame: {current_frame} - Time: {int(current_time)} - FPS: {int(current_frame // current_time) if current_time > 0 else 0.01}')
+
+            cv2.imshow(window_name, image.data)
+
+            if self._n_frames is not None:
+                k = cv2.waitKey(int(round(max(1./fps - (time.time() - start), 0), 3) * 1000))
+            else:
+                k = cv2.waitKey(1)
+            if k == ord('q') or k == ord('\x1b'):
+                break
+
+        cv2.destroyAllWindows()
+
+    @classmethod
+    def load_from_dir(cls, p):
+        return cls(Image.images_from_dir(p))
