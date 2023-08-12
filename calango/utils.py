17c923e00839d48804d12369b1cc47031f609d43
Joab Leite <leitejoab@gmail.com>
2023-08-12 04:00:25 -0300

    chore: added ImagePlot util

--- calango/utils.py
+++ calango/utils.py
@@ -1,6 +1,8 @@
 import base64
 import io
 from .settings import ON_COLAB_JUPYTER
+from matplotlib import pyplot as plt
+import math
 
 __all__ = ['show_local_mp4']
 
@@ -17,3 +19,30 @@ def show_local_mp4(file_name, width=640, height=480):
                     width, height, video_encoded.decode("ascii")
             )
     )
+
+
+class ImagePlot:
+    def __init__(self, fig_size=(13, 13), max_cols=4):
+        self.fig_size = fig_size
+        self.figure = plt.figure(figsize=fig_size)  # TODO: does it need to be global?
+        self._max_cols = max_cols
+        self._total_images = 0
+
+    @property
+    def current_row(self):
+        return math.ceil(self._total_images / self._max_cols)
+
+    @property
+    def current_col(self):
+        return ((self._total_images - 1) % self._max_cols) + 1
+
+    def plot(self, image, title=None, color='gray'):
+        try:
+            self._total_images += 1
+            plt.subplot(self._max_cols, self._max_cols, self._total_images)
+            plt.title(self.current_col if title is None else title)
+            plt.xticks([]), plt.yticks([])
+            plt.imshow(image.copy(), color)
+        except Exception as err:
+            self._total_images -= 1  # garante consistencia da quantidade de imagens
+            raise err
